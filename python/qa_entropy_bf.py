f199f4617a60577b7237c3f740d05dd9fc0ef14f
Ron Sadlier <ron.sadlier@gmail.com>
2014-06-27 11:35:13 -0400

    Improved efficiency of tests

--- python/qa_entropy_bf.py
+++ python/qa_entropy_bf.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2011 - 2014 Travis S. Humble - Oak Ridge National Laboratory
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
@@ -26,17 +26,19 @@ import random
 
 class qa_entropy_bf(gr_unittest.TestCase):
 
+  rounddigits = 3
+
   def setUp(self):
     self.tb = gr.top_block()
-    self.num_items = 25000;
-    self.window = (self.num_items/2);
-    self.precision = 3;
+    self.num_items = 15000
+    self.window = (self.num_items/2)
+    self.precision = 3
 
   def tearDown(self):
     self.tb = None
 
   def entropy_single(self, src_vector, expected_entropy, window):
-    src = blocks.vector_source_b(src_vector, 0x0, 1)
+    src = blocks.vector_source_b(src_vector)
     v2s = blocks.vector_to_stream(gr.sizeof_char, 1)
     entropy = qitkat.entropy_bf(window)
     data = blocks.vector_sink_f()
@@ -47,11 +49,11 @@ class qa_entropy_bf(gr_unittest.TestCase):
     obs = data.data()
     exp = expected_entropy
     for i in range(len(obs)):
-      self.assertAlmostEqual(exp, obs[i], self.precision)
+      self.assertAlmostEqual(exp, round(obs[i], self.rounddigits), self.precision)
 
   def entropy_joint(self, src_vector0, src_vector1, expected_entropy, window):
-    src0 = blocks.vector_source_b(src_vector0, 0x0, 1)
-    src1 = blocks.vector_source_b(src_vector1, 0x0, 1)
+    src0 = blocks.vector_source_b(src_vector0)
+    src1 = blocks.vector_source_b(src_vector1)
     v2s0 = blocks.vector_to_stream(gr.sizeof_char, 1)
     v2s1 = blocks.vector_to_stream(gr.sizeof_char, 1)
     entropy = qitkat.entropy_bf(window)
@@ -65,7 +67,7 @@ class qa_entropy_bf(gr_unittest.TestCase):
     obs = data.data()
     exp = expected_entropy
     for i in range(len(obs)):
-      self.assertAlmostEqual(exp, obs[i], self.precision)
+      self.assertAlmostEqual(exp,round(obs[i], self.rounddigits), self.precision)
 
   def test_001(self):
     # Test for alternating sequence

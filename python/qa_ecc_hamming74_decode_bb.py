213d924ff4a592787343ee92d194f47af4dcd0a0
Ron Sadlier <ron.sadlier@gmail.com>
2014-07-15 08:44:35 -0400

    Fixed copyright notices

--- python/qa_ecc_hamming74_decode_bb.py
+++ python/qa_ecc_hamming74_decode_bb.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2014 - 2014 Ronald Sadlier - Oak Ridge National Laboratory
-# 
+#
+# Copyright 2014 Ronald Sadlier - Oak Ridge National Laboratory
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
@@ -38,7 +38,7 @@ class qa_ecc_hamming74_decode_bb(gr_unittest.TestCase):
       90, 76, 60, 37, 85, 15, 0, 60, 105, 127, 127, 127)
     # Output
     # 1: 00000001, 2: 00000010 ... 15: 00001111
-    # 17: 00010001; 74: 01001010; 92: 01011100; 125: 01111101; 192: 11000000; 241: 11110001; 255: 11111111 
+    # 17: 00010001; 74: 01001010; 92: 01011100; 125: 01111101; 192: 11000000; 241: 11110001; 255: 11111111
     expected_result = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 74, 92, 125, 192, 241, 255)
     src = blocks.vector_source_b(src_data)
     hamming74_decode = qitkat.ecc_hamming74_decode_bb()
@@ -57,7 +57,7 @@ class qa_ecc_hamming74_decode_bb(gr_unittest.TestCase):
       90, 77, 60, 39, 85, 31, 0, 60, 105, 63, 127, 119)
     # Output
     # 1: 00000001, 2: 00000010 ... 15: 00001111
-    # 17: 00010001; 74: 01001010; 92: 01011100; 125: 01111101; 192: 11000000; 241: 11110001; 255: 11111111 
+    # 17: 00010001; 74: 01001010; 92: 01011100; 125: 01111101; 192: 11000000; 241: 11110001; 255: 11111111
     expected_result = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 74, 92, 125, 192, 241, 255)
     src = blocks.vector_source_b(src_data)
     hamming74_decode = qitkat.ecc_hamming74_decode_bb()
